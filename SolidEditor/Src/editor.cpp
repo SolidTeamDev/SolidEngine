e224ab4ae4ebc30ed699b5aafc0a0d7d58785ce9
LEFEBVRE Guillaume <gu.lefebvre@student.isartdigital.com>
2021-02-03 11:19:35 +0100

    Renderer context moved inside dll

--- SolidEditor/Src/editor.cpp
+++ SolidEditor/Src/editor.cpp
@@ -1,20 +1,9 @@
 #include "editor.hpp"
 
-#include "Rendering/OpenGL45/openGl45Renderer.hpp"
-
 namespace Solid
 {
     Editor::Editor()
     {
-        RendererParams rendererParams
-        {
-            {
-                "Solid Engine",
-                {1280,720}
-            }
-        };
-        renderer = new OpenGL45Renderer();
-        renderer->Initialize(rendererParams);
 
     }
 
