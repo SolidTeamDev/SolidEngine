9403e74bc6687ee5c64cb797352be22065ddf75e
Tristan <t.francillonne@hotmail.fr>
2021-02-08 23:55:41 +0100

    vector2 define function

--- SolidEngine/Include/Core/Maths/Vector/vector2.hpp
+++ SolidEngine/Include/Core/Maths/Vector/vector2.hpp
@@ -14,15 +14,97 @@ namespace Solid
         float y;
 
         constexpr Vec2() noexcept = default;
-        constexpr Vec2(const float& _value)noexcept;
-        constexpr Vec2(const float& _x,const float& _y = 0)noexcept;
-        constexpr Vec2(const Vec2& _copy)noexcept;
-        constexpr Vec2(const Vec3& _copy)noexcept;
-        constexpr Vec2(const Vec4& _copy)noexcept;
 
+        constexpr Vec2(const float &_value) noexcept;
 
+        constexpr Vec2(const float &_x, const float &_y = 0) noexcept;
+
+        constexpr Vec2(const Vec2 &_copy) noexcept;
+
+        constexpr Vec2(const Vec3 &_copy) noexcept;
+
+        constexpr Vec2(const Vec4 &_copy) noexcept;
 
         ~Vec2() = default;
+
+#pragma region Static Methods
+
+        static Vec2 zero;
+        static Vec2 up;
+        static Vec2 down;
+        static Vec2 left;
+        static Vec2 right;
+
+        static constexpr float dot(const Vec2 &_v1,const Vec2 &_v2) noexcept;
+
+        static constexpr float cross(const Vec2 &_v1,const Vec2 &_v2) noexcept;
+
+#pragma endregion
+#pragma region Methods
+
+        constexpr float sqrtLength() const noexcept;
+
+        constexpr float length() const noexcept;
+
+        Vec2 &scale(float _scale) noexcept;
+
+        constexpr Vec2 getScaled(float _scale) const noexcept;
+
+        Vec2 &unscale(float _scale) noexcept;
+
+        constexpr Vec2 getUnscaled(float _scale) const noexcept;
+
+        Vec2 &normalize() noexcept;
+
+        constexpr Vec2 getNormalize() const noexcept;
+
+
+#pragma endregion
+#pragma region Operator
+
+        constexpr Vec2 &operator = (const Vec2& vec) noexcept = default;
+
+        constexpr bool operator == (const Vec2 &_vec) const noexcept;
+
+        constexpr bool operator != (const Vec2 &_vec) const noexcept;
+
+        constexpr Vec2 operator + (const Vec2 &_vec) const noexcept;
+
+        constexpr Vec2 operator - (const Vec2 &_vec) const noexcept;
+
+        constexpr Vec2 operator * (const Vec2 &_vec) const noexcept;
+
+        constexpr Vec2 operator / (const Vec2 &_vec) const noexcept;
+
+        constexpr Vec2 &operator += const Vec2 &_vec) noexcept;
+
+        constexpr Vec2 &operator -= (const Vec2 &_vec) noexcept;
+
+        constexpr Vec2 &operator *= (const Vec2 &_vec) noexcept;
+
+        constexpr Vec2 &operator /= (const Vec2 &_vec) noexcept;
+
+        constexpr Vec2 operator + (const float &_value) const noexcept;
+
+        constexpr Vec2 operator - (const float &_value) const noexcept;
+
+        constexpr Vec2 operator * (const float &_value) const noexcept;
+
+        constexpr Vec2 operator / (const float &_value) const noexcept;
+
+        constexpr Vec2 &operator += (const float &_value) noexcept;
+
+        constexpr Vec2 &operator -= (const float &_value) noexcept;
+
+        constexpr Vec2 &operator *= (const float &_value) noexcept;
+
+        constexpr Vec2 &operator /= (const float &_value) noexcept;
+
+        constexpr float operator [] (const size_t _index) const noexcept;
+
+        constexpr float &operator [] (const size_t _index) noexcept;
+
+#pragma endregion
 
     };
 
--- SolidEngine/Src/Core/Vector/vector2.cpp
+++ SolidEngine/Src/Core/Vector/vector2.cpp
@@ -5,6 +5,14 @@
 
 namespace Solid
 {
+    Vec2 Vec2::zero{0,0};
+    Vec2 Vec2::up{0,1};
+    Vec2 Vec2::down{0,-1};
+    Vec2 Vec2::left{-1,0};
+    Vec2 Vec2::right{1,0};
+
+
+
     constexpr Vec2::Vec2(const float& _value) noexcept :
     x{_value},
     y{_value}
@@ -30,5 +38,198 @@ namespace Solid
     y{_copy.y}
     {}
 
+    constexpr float Vec2::dot(const Vec2& _v1,const Vec2 &_v2) noexcept
+    {
+        /*TODO*/
+        return 0;
+    }
+
+    constexpr float Vec2::cross(const Vec2& _v1,const Vec2 &_v2) noexcept
+    {
+        /*TODO*/
+        return 0;
+    }
+
+    constexpr float Vec2::sqrtLength() const noexcept
+    {
+        /*TODO*/
+        return 0;
+    }
+
+    constexpr float Vec2::length() const noexcept
+    {
+        /*TODO*/
+        return 0;
+    }
+
+    Vec2& Vec2::scale(float _scale) noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+
+    }
+
+    constexpr Vec2 Vec2::getScaled(float _scale) const noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+
+    }
+
+    Vec2& Vec2::unscale(float _scale) noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+
+    }
+
+    constexpr Vec2 Vec2::getUnscaled(float _scale) const noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+
+    }
+
+    Vec2& Vec2::normalize() noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+
+    }
+
+    constexpr Vec2 Vec2::getNormalize() const noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+    }
+
+    constexpr bool Vec2::operator==(const Vec2 &_vec) const noexcept
+    {
+        /*TODO*/
+        return false;
+    }
+
+    constexpr bool Vec2::operator!=(const Vec2 &_vec) const noexcept
+    {
+        /*TODO*/
+        return false;
+    }
+
+    constexpr Vec2 Vec2::operator+(const Vec2 &_vec) const noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+
+    }
+
+    constexpr Vec2 Vec2::operator-(const Vec2 &_vec) const noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+
+    }
+
+    constexpr Vec2 Vec2::operator*(const Vec2 &_vec) const noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+
+    }
+
+    constexpr Vec2 Vec2::operator/(const Vec2 &_vec) const noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+
+    }
+
+    constexpr Vec2 &Vec2::operator-=(const Vec2 &_vec) noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+
+    }
+
+    constexpr Vec2 &Vec2::operator*=(const Vec2 &_vec) noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+
+    }
+
+    constexpr Vec2 &Vec2::operator/=(const Vec2 &_vec) noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+
+    }
+
+    constexpr Vec2 Vec2::operator+(const float &_value) const noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+
+    }
+
+    constexpr Vec2 Vec2::operator-(const float &_value) const noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+
+    }
+
+    constexpr Vec2 Vec2::operator*(const float &_value) const noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+
+    }
+
+    constexpr Vec2 Vec2::operator/(const float &_value) const noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+
+    }
+
+    constexpr Vec2 &Vec2::operator+=(const float &_value) noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+
+    }
+
+    constexpr Vec2 &Vec2::operator-=(const float &_value) noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+
+    }
+
+    constexpr Vec2 &Vec2::operator*=(const float &_value) noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+
+    }
+
+    constexpr Vec2 &Vec2::operator/=(const float &_value) noexcept
+    {
+        /*TODO*/
+        return Vec2::zero;
+
+    }
+
+    constexpr float Vec2::operator[](const size_t _index) const noexcept
+    {
+        /*TODO*/
+        return 0;
+
+    }
+
+    /*constexpr float& Vec2::operator[](const size_t _index) noexcept
+    {
+        TODO
+    }*/
 
 }
