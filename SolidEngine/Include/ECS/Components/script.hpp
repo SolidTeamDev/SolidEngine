9727bcac087bab9e781dea0044e95d096048e393
LEFEBVRE Guillaume <gu.lefebvre@student.isartdigital.com>
2021-03-17 14:59:26 +0100

    Add gameobject pointer to script

--- SolidEngine/Include/ECS/Components/script.hpp
+++ SolidEngine/Include/ECS/Components/script.hpp
@@ -4,13 +4,19 @@ namespace Solid
 {
     class  Script
     {
+    protected:
+        GameObject* gameobject= nullptr;
     public:
+
         Script() = default;
+        Script(GameObject* _go) {gameobject = _go;}
         virtual ~Script() = default;
 
         virtual void Update() = 0;
         virtual void FixedUpdate() = 0;
         virtual void LateUpdate() = 0;
+
+        GameObject* GetGameObject(){return gameobject;}
 
     };
 }// !namespace
