b1ea5b8aa0683a556c03605f4ff03286b1bffd8c
LEFEBVRE Guillaume <gu.lefebvre@student.isartdigital.com>
2021-05-25 11:28:24 +0200

    [WORK] OnTrigger and OnContact

--- SolidEngine/Src/Physics/physicsEventCallback.cpp
+++ SolidEngine/Src/Physics/physicsEventCallback.cpp
@@ -2,17 +2,92 @@
 
 #include <PxRigidActor.h>
 #include "ECS/sceneGraphManager.hpp"
+#include "ECS/Components/scriptList.hpp"
+#include "Core/engine.hpp"
 #include "Core/Debug/log.hpp"
 
-void Solid::PhysicsEventCallback::onContact(const physx::PxContactPairHeader &pairHeader, const physx::PxContactPair *pairs, physx::PxU32 nbPairs)
+namespace Solid
 {
-    Log::Send("On contact",Log::ELogSeverity::DEBUG);
-}
+    void PhysicsEventCallback::onContact(const physx::PxContactPairHeader &pairHeader, const physx::PxContactPair *pairs, physx::PxU32 nbPairs)
+    {
+        Engine* engine = Engine::GetInstance();
+        for(size_t i = 0 ; i < nbPairs; i++)
+        {
+            Log::Send("On contact : " + std::to_string(nbPairs), Log::ELogSeverity::DEBUG);
+            GameObject* go1 = (GameObject*)pairHeader.actors[0]->userData;
+            GameObject* go2 = (GameObject*)pairHeader.actors[1]->userData;
 
-void Solid::PhysicsEventCallback::onTrigger(physx::PxTriggerPair *pairs, physx::PxU32 count)
-{
-    Log::Send("On trigger", Log::ELogSeverity::DEBUG);
-    GameObject* go1 = (GameObject*)pairs->triggerActor->userData;
-    GameObject* go2 = (GameObject*)pairs->otherActor->userData;
-    //TODO: get scripts and call trigger function
-}
+            physx::PxPairFlags flagStatus = pairs[i].events;
+
+            if (engine->ecsManager.GotComponent<ScriptList>(go1->GetEntity()))
+            {
+                std::vector<Script *> scriptListGo1 = engine->ecsManager.GetComponent<ScriptList>(
+                        go1->GetEntity()).GetAllScripts();
+
+                for (auto &script : scriptListGo1)
+                {
+                    if (flagStatus == physx::PxPairFlag::eNOTIFY_TOUCH_FOUND)
+                        script->OnContactEnter(go2);
+                    else if (flagStatus == physx::PxPairFlag::eNOTIFY_TOUCH_LOST)
+                        script->OnContactExit(go2);
+                }
+            }
+            if (engine->ecsManager.GotComponent<ScriptList>(go2->GetEntity()))
+            {
+                std::vector<Script *> scriptListGo2 = engine->ecsManager.GetComponent<ScriptList>(
+                        go2->GetEntity()).GetAllScripts();
+
+                for(auto& script : scriptListGo2)
+                {
+                    if(flagStatus == physx::PxPairFlag::eNOTIFY_TOUCH_FOUND)
+                        script->OnContactEnter(go1);
+                    else if(flagStatus == physx::PxPairFlag::eNOTIFY_TOUCH_LOST)
+                        script->OnContactExit(go1);
+                }
+            }
+        }
+    }
+
+    void PhysicsEventCallback::onTrigger(physx::PxTriggerPair *pairs, physx::PxU32 count)
+    {
+        Engine* engine = Engine::GetInstance();
+
+        for(size_t i = 0 ; i < count; i++)
+        {
+            Log::Send("On trigger : " + std::to_string(count), Log::ELogSeverity::DEBUG);
+            GameObject *go1 = (GameObject *) pairs[i].triggerActor->userData;
+            GameObject *go2 = (GameObject *) pairs[i].otherActor->userData;
+
+            physx::PxPairFlag::Enum flagStatus = pairs[i].status;
+
+            if (engine->ecsManager.GotComponent<ScriptList>(go1->GetEntity()))
+            {
+                std::vector<Script *> scriptListGo1 = engine->ecsManager.GetComponent<ScriptList>(
+                        go1->GetEntity()).GetAllScripts();
+
+                for (auto &script : scriptListGo1)
+                {
+                    if (flagStatus == physx::PxPairFlag::eNOTIFY_TOUCH_FOUND)
+                        script->OnTriggerEnter(go2);
+                    else if (flagStatus == physx::PxPairFlag::eNOTIFY_TOUCH_LOST)
+                        script->OnTriggerExit(go2);
+                }
+            }
+            if (engine->ecsManager.GotComponent<ScriptList>(go2->GetEntity()))
+            {
+                std::vector<Script *> scriptListGo2 = engine->ecsManager.GetComponent<ScriptList>(
+                        go2->GetEntity()).GetAllScripts();
+
+                for(auto& script : scriptListGo2)
+                {
+                    if(flagStatus == physx::PxPairFlag::eNOTIFY_TOUCH_FOUND)
+                        script->OnTriggerEnter(go1);
+                    else if(flagStatus == physx::PxPairFlag::eNOTIFY_TOUCH_LOST)
+                        script->OnTriggerExit(go1);
+                }
+            }
+        }
+    }
+} //!namespace
+
+
